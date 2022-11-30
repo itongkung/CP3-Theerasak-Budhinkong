77408cc0ca406d5f03e3e21a50175afee2560ac9
itongkung <119542047+itongkung@users.noreply.github.com>
2022-12-01 05:07:28 +0700

    Create Exercise4_Theerasak_b.py

---
+++ Exercise4_Theerasak_b.py
@@ -0,0 +1,10 @@
+A = 78
+B = 87
+C = 78
+D = 87
+
+print("--- Your Score ---")
+print("Foundation English > ",A)
+print("General Business > ",B)
+print("Introduction to Computer Systems > ",C)
+print("Computer Programming > ",D)
