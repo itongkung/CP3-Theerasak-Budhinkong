1463bd51f4fb49df9711dbc344be99ac8a797c90
itongkung <119542047+itongkung@users.noreply.github.com>
2022-12-02 06:59:38 +0700

    Create Exercise9_Theerasak_b.py

---
+++ Exercise9_Theerasak_b.py
@@ -0,0 +1,7 @@
+User = input("User : ")
+Passw = input("Pass : ")
+while not (User == 'admin' and Passw == '1234'):
+    User = input("Try User : ")
+    Passw = input("Try Pass : ")
+
+print("Wow!!!!!!!!!!!")
